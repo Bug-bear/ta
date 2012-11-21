a707280179ef9669e47770c94e774bfc3d08e90a
Ed <peng@dcs-bbk-ac-uk>
2012-11-21 08:34:18 +0000

    unfinished polyroot - not sure about the question

---
+++ PiJ/lab/16/PolyRoot.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class PolyRoot{
+	static final double PRECISION = 0.000001;
+	static final double LOW = -1000;
+	static final double HIGH = 1000;
+	
+	public static List<Double> eval(int[] poly){
+		return eval(poly, LOW, HIGH);
+	}
+	
+	private static List<Double> eval(int[] poly, double low, double high){
+		List<Double> ret = new ArrayList<Double>();
+		if(testRoot(testVal(poly, low))){
+			ret.add(low);
+		}
+		if(testRoot(testVal(poly, high))){
+			ret.add(high);
+		}
+		//if(low<high && ret.size()<=(poly.length-1) ((testVal(poly,low)*testVal(poly,high))<0 || ret.size()<=poly.length-1)){
+		if(low<high){
+			ret.addAll(eval(poly, low+1, high-1));
+		}
+		return ret;
+	}
+	
+	public static double testVal(int[] poly, double x){
+		double result = 0;
+		for(int i=0; i<poly.length; i++){
+			result += poly[i] * Math.pow(x, i);
+		}
+		return result;
+	}
+	
+	public static boolean testRoot(double x){
+		return Math.abs(x) < PRECISION;
+	}
+	
+	public static void main(String[] args){
+		int[] poly1 = {6, -5, 1};
+		//System.out.println(testRoot(poly, 3));
+		System.out.println(eval(poly1));
+		int[] poly2 = {-9, -6, 3};
+		System.out.println(eval(poly2));
+	}
+}
